58550b5c155b50a0effd57e0c0276d854ece7dd9
ThemaicolHD <roasalasmaicolstip@gmail.com>
2021-12-27 12:08:47 -0500

    venombot

---
+++ app.js
@@ -0,0 +1,110 @@
+// Supports ES6
+// import { create, Whatsapp } from 'venom-bot';
+const mensaje = require('./mensajes/steps.json')
+const venom = require('venom-bot');
+let backToMenu = false;
+let steps = 1;
+let sendMessage;
+let wellcomeMesage="¡Hola! Soy Mostro, tu asesor virtual de Emprendimiento SENA y te puedo mostrar una ruta para construir. \n\nConoce tus opciones para emprender con nosotros: \n\n1. Crear - Ideas de negocio 💡 \n2. Crecer - Empresas o unidades productivas 🚀 \n\nDigita la opción deseada.";
+venom
+  .create({
+    session: 'session-name', //name of session
+    multidevice: false // for version not multidevice use false.(default: true)
+  })
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+function start(client) {
+  client.onMessage((message) => {
+    body = message.body.toLowerCase();
+    console.log('este es el body', steps)
+    if (mensaje.STEP_1.includes(body) && steps == 1) {
+      steps = 2
+      client
+      .sendText(message.from, wellcomeMesage)
+      .then((result) => {
+        console.log('Result: ', result); //return object success
+      })
+      .catch((erro) => {
+        console.error('Error when sending: ', erro); //return object error
+      });
+      body= "";
+    }
+    else if ((mensaje.OPTION_1.includes(body) && steps == 2) || backToMenu) {
+      steps = 3
+      if (body == mensaje.OPTION_1[0] || body == mensaje.OPTION_1[1] || body == mensaje.OPTION_1[2]){
+        sendMessage = "¿tienes un producto que ya haya generado ventas? \n\nResponda Si o No";
+      }else 
+      if(body == mensaje.OPTION_1[3] || body == mensaje.OPTION_1[4]){
+        sendMessage = "¿tienes una empresa legalmente constituida? \n\nResponda Si o No";
+      }
+    
+      body = "";
+      client
+      .sendText(message.from, sendMessage)
+      .then((result) => {
+        console.log('Result: ', result); //return object success
+      })
+      .catch((erro) => {
+        console.error('Error when sending: ', erro); //return object error
+      });
+    }
+    else if (mensaje.RESPUESTA.includes(body) && steps == 3) {
+      if (body == mensaje.RESPUESTA[0]){
+        sendMessage = "Genial, a continuacion te adjunto el link al cual puedes dirigirte para realizar el formulario: \n\nhttps://www.youtube.com/watch?v=mCdA4bJAGGk";
+      }
+      else if(body == mensaje.RESPUESTA[1]){
+        sendMessage = "¿tienes una idea de negocio?";
+      }
+      else if(body == mensaje.OPTION_1[6]){
+        steps = 1
+        backToMenu = true;
+        sendMessage = wellcomeMesage;       
+      }
+      
+      client
+      .sendText(message.from, sendMessage)
+      .then((result) => {
+        console.log('Result: ', result); //return object success
+        if(body == "regresar"){         
+        return;
+        }
+      })
+      .catch((erro) => {
+        console.error('Error when sending: ', erro); //return object error
+        if(body == "regresar"){
+          return;
+          }
+      });
+      body= "";
+    }
+
+
+
+    else{
+      body= "";
+      console.log(mensaje.ERROR[0])
+      client
+      .sendText(message.from,mensaje.ERROR[0])
+      .then((result) => {
+        console.log('Result: ', result); //return object success
+      })
+      .catch((erro) => {
+        console.error('Error when sending: ', erro); //return object error
+      });
+
+      client
+      .sendText(message.from)
+      .then((result) => {
+        console.log('Result: ', result); //return object success 
+        return
+      })
+      .catch((erro) => {
+        console.error('Error when sending: ', erro); //return object error
+        return
+      });
+    }
+  });
+}
